664b31ec90f434435a38afc10e6bddd9efef49e9
c0a24030 <c0a240300f@edu.teu.ac.jp>
2025-04-22 14:55:36 +0900

    練習４：衝突判定

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -49,7 +49,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-        screen.blit(bg_img, [0, 0]) 
+        screen.blit(bg_img, [0, 0])
+
+        # こうかとんれくととばくだんれくとがかさなったら
+        if kk_rct.colliderect(bb_rct): 
+            return
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
