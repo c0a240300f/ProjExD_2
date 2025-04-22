f8768b2621e1c6599b6372e3dd4e8c72d50319ab
c0a24030 <c0a240300f@edu.teu.ac.jp>
2025-04-22 14:50:40 +0900

    練習３：跳ね返る

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -11,6 +11,22 @@ DELTA = {
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    引数はこうかとんRectまたは爆弾Rect
+    戻り値は判定結果タプル(横, 縦)
+    画面内ならFalse画面外ならTrue
+    """
+    yoko = True
+    tate = True
+    # 縦横方向判定
+    if rct.left < 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -52,8 +68,19 @@ def main():
         #    sum_mv[0] += 5
 
         kk_rct.move_ip(sum_mv)
+        # こうかとん画面外挙動
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx, vy)
+        # 爆弾の挙動
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
